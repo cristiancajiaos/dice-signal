3b572270edd83752dac1e2088f7540f677cdbe30
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-24 17:23:06 -0400

    Añadidos signals para contar total de tiros

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faDice } from '@fortawesome/free-solid-svg-icons';
+import { faDice, faDiceFive, faDiceFour, faDiceOne, faDiceSix, faDiceThree, faDiceTwo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-layout',
@@ -12,7 +12,22 @@ export class Layout implements OnInit {
 
   public diceIcon: IconDefinition = faDice;
 
+  public diceOneIcon: IconDefinition = faDiceOne;
+  public diceTwoIcon: IconDefinition = faDiceTwo;
+  public diceThreeIcon: IconDefinition = faDiceThree;
+  public diceFourIcon: IconDefinition = faDiceFour;
+  public diceFiveIcon: IconDefinition = faDiceFive;
+  public diceSixIcon: IconDefinition = faDiceSix;
+
+
   public diceValue: WritableSignal<number> = signal(0);
+
+  public oneCount: WritableSignal<number> = signal(0);
+  public twoCount: WritableSignal<number> = signal(0);
+  public threeCount: WritableSignal<number> = signal(0);
+  public fourCount: WritableSignal<number> = signal(0);
+  public fiveCount: WritableSignal<number> = signal(0);
+  public sixCount: WritableSignal<number> = signal(0);
 
   ngOnInit(): void {
 
@@ -21,5 +36,37 @@ export class Layout implements OnInit {
   public rollDice(): void {
     const diceValue = Math.floor(Math.random() * 6) + 1;
     this.diceValue.set(diceValue);
+
+    switch(diceValue) {
+      case 1: {
+        this.oneCount.update((value) => value + 1);
+        break;
+      }
+
+      case 2: {
+        this.twoCount.update((value) => value + 1);
+        break;
+      }
+
+      case 3: {
+        this.threeCount.update((value) => value + 1);
+        break;
+      }
+
+      case 4: {
+        this.fourCount.update((value) => value + 1);
+        break;
+      }
+
+      case 5: {
+        this.fiveCount.update(value => value + 1);
+        break;
+      }
+
+      case 6: {
+        this.sixCount.update(value => value + 1);
+        break;
+      }
+    }
   }
 }
